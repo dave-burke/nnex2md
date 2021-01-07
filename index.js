32076d9ca7d1726de848514af8afa02c7467b7a2
Dave Burke <dburke84@gmail.com>
2021-01-06 22:03:20 -0600

    Handle en-media references

--- index.js
+++ index.js
@@ -2,26 +2,15 @@ const fs = require('fs')
 const xml2js = require('xml2js')
 const TurndownService = require('turndown')
 
-const turndownService = new TurndownService({
-  bulletListMarker: '-',
-  emDelimiter: '*',
-})
-
-turndownService.addRule('em-media', {
-  filter: ['en-media'],
-  replacement(content) {
-    return '' // TODO
-  }
-})
-
 async function main() {
   const xml = fs.readFileSync('evernote.nnex')
   const js = await(xml2js.parseStringPromise(xml));
   try {
     const notes = mapNotes(js['nixnote-export'])
-    const test = notes.find(note => note.content.length > 300 && note?.resources?.[0].data?.body !== undefined)
+    turndownService.use(enMediaPlugin(notes))
+    const test = notes.find(note => note.guid === 'f6d96c9b-0c5e-4699-aee7-4796399d8951')
     writeNoteToFile(test)
-    saveResources(test)
+    saveResourcesAsFiles(test)
   } catch(err) {
     console.log(err)
   }
@@ -56,13 +45,52 @@ function writeNoteToFile(note) {
   console.log(markdown)
 }
 
-function saveResources(note) {
+function saveResourcesAsFiles(note) {
   for(const resource of note.resources) {
     const mimeType = resource.mime
     const bytes = resource?.data?.bytes
     const fileName = resource?.attributes?.fileName
     if(fileName !== undefined) {
       console.log(`Save ${mimeType} as ${fileName}`)
+    }
+  }
+}
+
+// These are just my personal preferences
+const turndownService = new TurndownService({
+  bulletListMarker: '-',
+  emDelimiter: '*',
+})
+
+/*
+This creates a Turndown plugin that is aware of all the notes in our file.
+
+Instead of `img` nodes, the note content references images like this:
+
+<en-media ... hash="abc123"></en-media>
+
+Where the hash (abc123 in this case) references note.resource.data.bodyHash
+of some resource on the same note.
+
+When we encounter an en-media node, we need to find the resource and build a
+markdown image tag with a reference to the file.
+*/  
+const enMediaPlugin = (notes) => {
+  // Proactively map hashes to avoid lookups for every node
+  const resourcesByHash = notes
+    .map(note => note.resources).flat()
+    .filter(resource => resource?.data.bodyHash !== undefined)
+    .reduce((map, resource) => {
+      map.set(resource.data.bodyHash, resource)
+      return map
+    }, new Map())
+  return (turndown) => {
+    turndown.rules.blankRule.replacement = function(content, node) {
+      if(node.nodeName === 'EN-MEDIA') {
+        const hash = node.attributes.getNamedItem('hash').value
+        const resource = resourcesByHash.get(hash)
+        return `![${hash}](assets/${resource.fileName})`
+      }
     }
   }
 }
@@ -76,6 +104,7 @@ class Notebook {
 
 class Note {
   constructor(noteDom, notebook) {
+    this.guid = noteDom['Guid'][0]
     this.title = noteDom['Title'][0]
     this.notebook = notebook
     this.content = noteDom['Content'][0]
@@ -91,6 +120,12 @@ class NoteResource {
     this.mime = noteResourceDom?.['Mime']?.[0]
     this.data = new Data(noteResourceDom['Data'][0])
     this.attributes = new ResourceAttributes(noteResourceDom['ResourceAttributes']?.[0])
+  }
+
+  get fileName() {
+    const hash = this.data.bodyHash
+    const fileExtension = this.mime.split('/')[1]
+    return this.attributes?.fileName ?? `${hash}.${fileExtension}`
   }
 }
 
